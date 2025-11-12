272746d466f034dfc059e3d771b9aee0dfc3e22c
Alexander Ojeniweh <ojeniwehalexander@gmail.com>
2025-11-12 09:39:01 +0100

    fixes

--- src/server.ts
+++ src/server.ts
@@ -13,7 +13,7 @@ console.log("DB_HOST:", process.env.DB_HOST);
 console.log("DB_USER:", process.env.DB_USER);
 console.log("DB_NAME:", process.env.DB_NAME);
 console.log("DB_PASSWORD:", process.env.DB_PASSWORD);
-console.log("JWT_SECRET:", process.env.JWT_SECRET ? "SET" : "MISSING");
+console.log("JWT_SECRET:", process.env.JWT_SECRET);
 console.log("=================");
 
 const startServer = async () => {
